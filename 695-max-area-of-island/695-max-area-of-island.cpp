a2332ac94a8d5cf5eb4efa39ceeb60111afbf8c6
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-07-15 10:00:13 +0530

    Time: 39 ms (28.66%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -19,7 +19,7 @@ class Solution
     {
         int n = grid.size();
         int m = grid[0].size();
-        int ans = 0, x =0;
+        int ans = 0;
         for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
             {
