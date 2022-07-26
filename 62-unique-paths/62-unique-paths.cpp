c37cc6cd8768d80891fa647d2eb13c6b68f97b3e
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-07-26 23:13:50 +0530

    Time: 6 ms (12.28%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,14 +10,8 @@ class Solution
             {
                 if(i == 0 || j == 0)
                     dp[i][j] = 1;
-            }
-        }
-
-        for(int i=1; i<m; i++)
-        {
-            for(int j=1; j<n; j++)
-            {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
