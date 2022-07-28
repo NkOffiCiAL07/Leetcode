2cf2b20686604f6ce681230cb76262bd53bf95b0
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-07-28 22:37:42 +0530

    Time: 1276 ms (23.23%), Space: 163.2 MB (64.82%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -3,26 +3,29 @@ class Solution
     public:
     vector<int> adj[200001];
     bool vis[200001];
-    void dfs(int u, int v)
-    {
-        vis[u] = 1;
-        for(int child : adj[u])
-        {
-            if(!vis[child])
-                dfs(child, v);
-        }
-    }
     
-    bool validPath(int n, vector<vector<int>> &edges, int source, int destination) 
+    bool validPath(int n, vector<vector<int>>& edges, int s, int d) 
     {
-        for(auto v : edges)
+        for(auto a : edges)
         {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
+            adj[a[0]].push_back(a[1]);
+            adj[a[1]].push_back(a[0]);
         }
         
-        dfs(source, destination);
+        queue<int> q;
+        q.push(s);
+        while(!q.empty())
+        {
+            int v = q.front();
+            vis[v] = true;
+            q.pop();
+            for(auto k : adj[v])
+            {
+                if(!vis[k])
+                    q.push(k);
+            }
+        }
         
-        return vis[destination];
+        return vis[d];
     }
 };
