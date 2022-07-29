f750af97aef26a77d7ae07300f0eea03e9ab3691
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-07-29 21:43:36 +0530

    Time: 15 ms (41.61%), Space: 10.4 MB (15.21%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{
+    public:
+    vector<int> adj[1001];
+    bool dfs(int v, vector<bool> &vis, int par = -1)
+    {
+        if(vis[v])
+            return 1;
+        
+        vis[v] = 1;
+        
+        for(auto child : adj[v])
+        {
+            if(child != par && dfs(child, vis, v))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {
+        
+        int n = edges.size();
+        for(auto a : edges)
+        {
+            vector<bool> vis(n+1);
+            adj[a[0]].push_back(a[1]);
+            adj[a[1]].push_back(a[0]);
+            
+            if(dfs(a[0], vis))
+            {
+                return a;
+            }
+        }
+        
+        return {};
+    }
+    
+};
