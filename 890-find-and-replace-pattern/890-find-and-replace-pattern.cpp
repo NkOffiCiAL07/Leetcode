d9be3e6c5bd90e74a56993e287bc4b9bf8a3f9c2
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-07-29 09:15:54 +0530

    Time: 15 ms (7.40%), Space: 8.5 MB (53.91%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+    public:
+    string encodeString(string s) 
+    {
+        unordered_map<char, int> m;
+        string res = "";
+        int i = 0;
+
+        for(auto it : s) 
+        {
+            if(m.find(it) == m.end())
+                m[it] = i++;
+            
+            res += to_string(m[it]);
+        }
+        return res;
+    }
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) 
+    {
+        string encodedString = encodeString(pattern); 
+        vector<string> v;
+
+        for(auto it : words) 
+        {
+            string s = encodeString(it);
+            if(s == encodedString) 
+            {
+                v.push_back(it);
+            }
+        }
+        return v;
+    }
+};
