bf0cddb097cd71eaf201e3a5005f2147de792bd7
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-04 21:57:25 +0530

    Time: 18 ms (14.92%), Space: 15 MB (20.23%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -2,12 +2,16 @@ class Solution
 {
     void dfs(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& m) {
         if(!root) return;
-        if(root->left) {
+        if(root->left) 
+        {
             m.insert({root->left, root});
         }
-        if(root->right) {
+        
+        if(root->right) 
+        {
             m.insert({root->right, root});
         }
+        
         dfs(root->left, m);
         dfs(root->right, m);
     }
@@ -26,8 +30,12 @@ class Solution
             int len = q.size();
             while(len--) 
             {
-                TreeNode* curr = q.front(); q.pop();
-                if(!k) res.push_back(curr->val);
+                TreeNode* curr = q.front(); 
+                q.pop();
+                
+                if(!k) 
+                    res.push_back(curr->val);
+                
                 visited[curr] = true;
                 if(curr->left && !visited[curr->left]) 
                 {
