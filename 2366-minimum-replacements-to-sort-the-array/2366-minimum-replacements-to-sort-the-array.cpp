1492e4b4aedb927f0f7882294a123d28ef6005bc
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-06 22:49:42 +0530

    Time: 177 ms (50.00%), Space: 54.7 MB (75.00%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     long long minimumReplacement(vector<int>& nums) {
         
-        long long ans = 0, sum = 0;
+        long long ans = 0;
         for(int i=nums.size()-2; i>=0; i--)
         {
-            sum += nums[i];
             if(nums[i] > nums[i+1])
             {
                 if(nums[i] % nums[i+1] == 0)
@@ -15,8 +14,9 @@ public:
                 }
                 else
                 {
-                    ans += ((nums[i] + nums[i+1] - 1) / nums[i+1]) - 1;
-                    nums[i] = nums[i] / ((nums[i]  + nums[i+1] - 1) / nums[i+1]);
+				    int temp = ((nums[i]  + nums[i+1] - 1) / nums[i+1]);
+                    ans += temp - 1;
+                    nums[i] = nums[i] / temp;
                 }
             }
         }
