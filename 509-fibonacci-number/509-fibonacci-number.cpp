4044f46d7cb863b185a86c2177507be5b659f7c3
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-25 17:26:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.79%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -9,6 +9,10 @@ class Solution
         if(n == 1)
             return 1;
         
-        return fib(n-1) + fib(n-2);
+        int dp[n+1]; dp[0] = 0; dp[1] = 1;
+        for(int i=2; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
     }
 };
