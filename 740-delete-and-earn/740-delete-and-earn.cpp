696f7a994de9f2cc148924ce974a889c20231fa8
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-27 16:59:05 +0530

    Time: 20 ms (64.74%), Space: 11.8 MB (87.36%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int> & nums) 
+    {
+        int m = -1;
+        for(auto x : nums) 
+            m = max(m, x);
+
+        vector<int> arr(m+1, 0);
+        for(auto x : nums) 
+            arr[x]++;
+        
+        for(int i=0; i<=m; i++) 
+            arr[i] = i*arr[i];
+
+        vector<int> dp(m+1, 0);
+        dp[0] = 0;
+        dp[1] = arr[1];
+
+        for(int i=2; i<=m; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + arr[i]);
+        }
+        
+        return dp[m];
+    }
+};
