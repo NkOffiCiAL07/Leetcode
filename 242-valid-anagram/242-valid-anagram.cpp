987f82f0e270923411a03334cd6b5c38b292b68f
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-30 14:54:04 +0530

    Time: 131 ms (5.47%), Space: 23.6 MB (8.57%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,5 +1,6 @@
-class Solution {
-public:
+class Solution 
+{
+    public:
     bool isAnagram(string s, string t) {
         multiset<char> m1(s.begin(), s.end());
         multiset<char> m2(t.begin(), t.end());
