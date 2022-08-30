7abc4ec5ad9d1d8a8dbe3a07577d1b2a2166683a
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-30 15:27:16 +0530

    Time: 11 ms (62.95%), Space: 13.7 MB (71.46%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,22 +1,19 @@
-class Solution 
-{
-    public:
-    int maxProduct(vector<int> &nums) 
-    {
-      int ans = nums[0];
-      int mx = ans;
-      int mn = ans;
-      for(int i=1; i<nums.size(); i++)
-      {
-          if(nums[i] < 0)
-          {
-              swap(mx, mn);
-          }
-              mx = max(nums[i], mx * nums[i]);
-              mn = min(nums[i], mn * nums[i]);
-              ans = max(ans, mx);              
-      }
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int mx = nums[0], mn = nums[0];
         
-       return ans;
+        int ans = nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(nums[i] < 0)
+                swap(mx, mn);
+            
+            mx = max(nums[i], mx * nums[i]);
+            mn = min(nums[i], mn * nums[i]);
+            ans = max(ans, mx);
+        }
+        
+        return ans;
     }
 };
