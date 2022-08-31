710972fac5be5fa9bf3d99eb3bdb1711a96d4d20
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-08-31 08:56:27 +0530

    Time: 40 ms (92.14%), Space: 27.6 MB (12.26%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,19 +1,22 @@
-class Solution 
-{
-    public:
-    int get_index(vector<int>& nums, int start, int end, int &target)
-    {
-        if(start > end) return -1;
-        int mid = start + (end - start) / 2;
-        if(target == nums[mid]) 
-            return mid;
-        else if(target < nums[mid]) 
-            return get_index(nums, start, mid - 1, target);
-        return get_index(nums, mid + 1, end, target);           
-    }
-    
-    int search(vector<int> &nums, int target) 
-    {
-        return get_index(nums, 0, nums.size() - 1, target);
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while(l <= r)
+        {
+            int m = (l + r) / 2;
+            if(nums[m] < target)
+            {
+                l = m + 1;
+            }
+            else if(nums[m] > target)
+            {
+                r = m - 1;
+            }
+            else
+                return m;
+        }
+        
+        return -1;
     }
 };
