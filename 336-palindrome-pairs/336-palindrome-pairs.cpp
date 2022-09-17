f0d74496d1e740b0d088fb45685cdab038fc9a31
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-09-17 13:03:09 +0530

    Time: 986 ms (85.96%), Space: 599.3 MB (15.75%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -5,17 +5,17 @@ struct TrieNode {
 };
 
 class Solution {
-    TrieNode root; // Suffix trie
+    TrieNode root; 
     void add(string &s, int i) {
         auto node = &root;
         for (int j = s.size() - 1; j >= 0; --j) {
-            if (isPalindrome(s, 0, j)) node->palindromeIndexes.push_back(i); // A[i]'s prefix forms a palindrome
+            if (isPalindrome(s, 0, j)) node->palindromeIndexes.push_back(i);
             int c = s[j] - 'a';
             if (!node->next[c]) node->next[c] = new TrieNode();
             node = node->next[c];
         }
         node->index = i;
-        node->palindromeIndexes.push_back(i); // A[i]'s prefix is empty string here, which is a palindrome.
+        node->palindromeIndexes.push_back(i);
     }
     
     bool isPalindrome(string &s, int i, int j) {
@@ -33,14 +33,11 @@ public:
             auto node = &root;
             for (int j = 0; j < s.size() && node; ++j) {
                 if (node->index != -1 && node->index != i && isPalindrome(s, j, s.size() - 1)) ans.push_back({ i, node->index }); 
-                // A[i]'s prefix matches this word and A[i]'s suffix forms a palindrome
+                
                 node = node->next[s[j] - 'a'];
             }
             if (!node) continue;
             for (int j : node->palindromeIndexes) { 
-                // A[i] is exhausted in the matching above. 
-                // If a word whose prefix is palindrome after matching its suffix with A[i], 
-                // then this is also a valid pair
                 if (i != j) ans.push_back({ i, j });
             }
         }
