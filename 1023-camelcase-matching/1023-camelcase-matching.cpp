e3d78f11640581899b058e549a873f736ed0a75b
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-10-05 18:26:32 +0530

    Time: 6 ms (20.51%), Space: 7.7 MB (6.78%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -1,22 +1,72 @@
 class Solution {
 public:
-    
-    bool isSubSeq(string &s, string &t) {
-        int j = 0;
-        for(char c:s) {
-            if(c >= 'A' && c <= 'Z' && c != t[j]) return false;
-            if(c == t[j]) j++;
+	struct node{
+		bool isEnd;
+		struct node* link[52];
+		node(){
+			isEnd=false;
+			for(int i=0;i<52;i++)
+				link[i]=NULL;
+		}
+	};
+	struct node* start=new node();
+	void insertTrie(const string &s){
+		struct node* ptr=start;
+		int n=s.size();
+		for(int i=0;i<n;i++){
+			if(s[i]>='a' && s[i]<='z'){
+				if(ptr->link[s[i]-'a']==NULL){
+					ptr->link[s[i]-'a']=new node();
+				}
+				ptr=ptr->link[s[i]-'a'];
+			}
+			else{
+				if(ptr->link[s[i]-'A'+26]==NULL){
+					ptr->link[s[i]-'A'+26]=new node();
+				}
+				ptr=ptr->link[s[i]-'A'+26];	
+			}
+		}
+		ptr->isEnd=1;
+	}
+	int findTrie(const string &s){
+		struct node* ptr=start;
+		int n=s.size();
+		for(int i=0;i<n;i++){
+			if(s[i]>='a' && s[i]<='z'){
+				if(ptr->link[s[i]-'a']!=NULL){
+					ptr=ptr->link[s[i]-'a'];
+                }
+			}
+			else{
+				if(ptr->link[s[i]-'A'+26]!=NULL){
+					ptr=ptr->link[s[i]-'A'+26];
+                }	
+                else return false;
+			}
+		}
+		return (ptr->isEnd);
+	}
+
+    bool check(string s,string t){
+        int i=0,j=0;
+        while(j<t.size() && i<s.size()){
+            if(s[i]==t[j]) i++,j++;
+            else if(s[i]>='a' && s[i]<='z') i++;
+            else return false;
         }
-        return j == t.size();
+        while(i<s.size()){
+            if(s[i]>='a' && s[i]<='z') i++;
+                else return false;
+        }
+        return (j==t.size());
     }
-    
-    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+    vector<bool> camelMatch(vector<string>& queries, string pat) {
         vector<bool> ans;
-        
-        for(auto &q: queries) {
-            ans.push_back(isSubSeq(q, pattern));
+        insertTrie(pat);
+        for(auto i:queries){
+            ans.push_back(findTrie(i));
         }
-        
         return ans;
     }
-};
+}; 
