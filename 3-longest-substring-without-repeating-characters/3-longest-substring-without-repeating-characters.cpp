5bb7c81dacc9641ba339df22c01057390b6e30b8
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-10-07 23:22:10 +0530

    Time: 20 ms (72.15%), Space: 8.3 MB (76.52%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,21 +5,16 @@ class Solution
     {
         vector<int> map(256, -1);
 
-        int left = 0, right = 0;
-        int n = s.size();
-        int len = 0;
-        while(right < n)
+        int left = 0, ans = 0;
+        for(int i=0; i<s.size(); i++)
         {
-            if(map[s[right]] != -1)
-            {
-                left = max(map[s[right]] + 1, left);
-            }
+            if(map[s[i]] != -1)
+                left = max(map[s[i]] + 1, left);
 
-            map[s[right]] = right;
-            len = max(len, right-left+1);
-            right++;
+            map[s[i]] = i;
+            ans = max(ans, i-left+1);
         }
 
-        return len;
+        return ans;
     }
 };
