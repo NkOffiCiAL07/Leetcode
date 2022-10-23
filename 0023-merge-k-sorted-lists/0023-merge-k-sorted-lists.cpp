7569aa1dfdf1a6aa24bcbd35e5c98aa5cc181b1f
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-10-23 23:53:15 +0530

    Time: 64 ms (34.46%), Space: 14.5 MB (6.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,32 +10,33 @@
  */
 
 struct MyComp{
-    bool operator() (ListNode* &a, ListNode* &b){
-        return a->val > b->val;
+    bool operator() (const ListNode* a, const ListNode* b) const{
+        return a->val < b->val;
     }
 };
 
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, MyComp> pq;
+        multiset<ListNode*, MyComp> pq;
         for(auto list : lists){
             if(list){
-                pq.push(list);
+                pq.insert(list);
             }
         }
         
         ListNode *res = new ListNode(0);
         ListNode *ans = res;
-        while(!pq.empty()){
-            ListNode *temp = pq.top();
-            pq.pop();
+        while(pq.size()){
+            auto it = pq.begin();
+            ListNode *temp = (*it);
+            pq.erase(it);
             
             ans->next = temp;
             ans = ans->next;
             
             if(temp->next){
-                pq.push(temp->next);
+                pq.insert(temp->next);
             }
         }
         
