31f69238a294b355bb787959f2a7dba442818298
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-10-23 09:40:41 +0530

    Time: 713 ms (23.08%), Space: 76.9 MB (76.92%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        int l = *min_element(nums.begin(),nums.end());
+        int r = *max_element(nums.begin(),nums.end());
+        long long ans = 1e18;
+        while (r >= l) {
+            long long mid1 = l + (r - l) / 3;
+            long long mid2 = r - (r - l) / 3;
+            
+            long long a1 = 0, a2 = 0;
+            
+            for(int i=0;i<nums.size();i++){
+                a1 += (abs(mid1 - nums[i]) * cost[i]);
+                a2 += (abs(mid2 - nums[i]) * cost[i]);
+            }
+            
+            if(a1 < a2){
+                ans = min(ans,a1);
+                r = mid2-1;
+            }else{
+                ans = min(ans,a2);
+                l = mid1+1;
+            }
+        }
+        return ans;
+    }
+};
