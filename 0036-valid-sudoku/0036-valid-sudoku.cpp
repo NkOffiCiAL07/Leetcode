4c50fd2e12437f1691da0fc27533686ed49e84d2
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-10-23 11:11:05 +0530

    Time: 63 ms (15.86%), Space: 20.4 MB (26.24%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,46 +1,23 @@
-class Solution {
-public:
+class Solution 
+{
+    public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        for(int i=0; i<n; i++){
-            set<int> s1, s2;
-            for(int j=0; j<n; j++){
-                if(board[i][j] != '.'){
-                    if(!s1.count(board[i][j]-'0')){
-                        s1.insert(board[i][j]-'0');
-                    }
-                    else
-                        return false;
-                }
+        vector<set<int>> rows(9), cols(9), blocks(9);
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 
-                if(board[j][i] != '.'){
-                    if(!s2.count(board[j][i]-'0')){
-                        s2.insert(board[j][i]-'0');
-                    }
-                    else
-                        return false;
-                }
-            }
-            
-            
-            for(int i=0; i<n; i+=3){
-                for(int j=0; j<n; j+=3){
-                    set<int> s3, s4;
-                    for(int k=0; k<=2; k++){
-                        for(int l=0; l<=2; l++){
-                           if(board[i+k][j+l] != '.'){
-                            if(!s3.count(board[i+k][j+l]-'0')){
-                                s3.insert(board[i+k][j+l]-'0');
-                            }
-                            else
-                                return false;
-                           }                         
-                        }
-                    } 
-                }
+                if (board[i][j] == '.') continue;
+                
+                int curr = board[i][j] - '0';
+                if (rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) 
+                    return false;
+                
+                rows[i].insert(curr);
+                cols[j].insert(curr);
+                blocks[(i/3)*3+j/3].insert(curr);
             }
         }
-        
         return true;
     }
 };
