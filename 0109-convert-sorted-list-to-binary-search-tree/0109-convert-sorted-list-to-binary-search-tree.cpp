0813783b6751772470c261b4ec9c8297916bf841
NISHANT KUMAR <nishantkumar19041@gmail.com>
2022-10-24 11:50:45 +0530

    Time: 48 ms (56.42%), Space: 28.3 MB (65.90%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -25,24 +25,23 @@ public:
     {
         if(!head)
             return NULL;
-        if(!head->next)
-        {
+        
+        if(!head->next){
             return new TreeNode(head->val);
         }
-        ListNode *slow=head,*fast=head->next;
-        while(fast->next&&fast->next->next)
-        {
-            slow=slow->next;
-            fast=fast->next->next;
-            
+        
+        ListNode *slow = head, *fast = head->next;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;    
         }
-        ListNode *mid=slow->next;
-        slow->next=NULL;
         
-        TreeNode *root=new TreeNode(mid->val);
-        root->left=sortedListToBST(head);
-        root->right=sortedListToBST(mid->next);
+        ListNode *mid = slow->next;
+        slow->next = NULL;
+        
+        TreeNode *root = new TreeNode(mid->val);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
         return root;
-        
     }
 };
